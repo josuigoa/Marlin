4587e8c75fe0aec06573c4776cd86fb5e15dd717
josu.igoa <jigoa@indaba.lks.es>
2019-01-21 15:47:04 +0100

    enable auto leveling

--- Marlin/config/hephestos_zum_heated_bed/Configuration.h
+++ Marlin/config/hephestos_zum_heated_bed/Configuration.h
@@ -365,7 +365,7 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 
 //============================= Bed Auto Leveling ===========================
 
-//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
+#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
 //#define Z_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #ifdef ENABLE_AUTO_BED_LEVELING
